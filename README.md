76a40b7b8c19a9be27994240b8d56d7e7444222b
xminarikd <32222554+xminarikd@users.noreply.github.com>
2019-03-17 18:57:41 +0100

    minor changes in doc

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # A* algoritmus
+
 Tento projekt je zadanim na predmet umela inteligencia UI.
 Zadanim projektu je riesit hlavolam (8puzzle) pomocou algoritmu A*
 
 Dokumentacia v podobe JavaDoc sa nachadza v adresari doc a hlavny program v adresari src.
 Pricom program je spustatelny z triedy Runner, kde su vytvorene testovacie scenare.
+
+##Autor
+* Daniel Minarik - FIIT STU - UI - 2.rocnik
--- src/Runner.java
+++ src/Runner.java
@@ -15,9 +15,10 @@ public class Runner {
 
         test1();
         test1_2();
-//        test2();
-//        test3();
-//        test4();
+        test2();
+        test3();
+        test4();
+        test5();
     }
 
     /**
@@ -78,6 +79,15 @@ public class Runner {
         ciel =new byte[][]{ {0, 1, 2, 3, 4}, {5, 6, 7, 8, 9}};
         getblank();
         solve(5,2,1);
+    }
+    /*
+    Neexistuje riesenie
+     */
+    public static void test5(){
+        start = new byte[][]{{8, 1, 2}, {0, 4, 3}, {7, 6, 5}};
+        ciel =new byte[][]{ {0, 1, 2,}, {3, 4, 5}, {6, 7, 8}};
+        getblank();
+        solve(3,3,1);
     }
 
 
